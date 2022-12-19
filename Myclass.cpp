e88422c40180c410bb18004a1c6a7b764f508b38
alirezarahatloo <rahatlooalireza@gmail.com>
2022-12-19 18:58:35 +0330

    Update Myclass.cpp

--- Myclass.cpp
+++ Myclass.cpp
@@ -1,1 +1,21 @@
+#include "Myclass.h"
+#include <iostream>
+using namespace std;
 
+
+MyClass::MyClass(int a,int b):regVar(a),constVar(b)
+{
+    //regVar   = a ;
+    //constVar = b ;
+    cout << regVar << "\n" << constVar << "\n" ;
+}
+
+void shift(MyClass &p,int step)
+{
+    p.regVar +=step;
+    cout << p.regVar << "\n";
+}
+void MyClass::printInfo()
+{
+    cout << this->regVar; //The this stores the address of the current object.
+}
