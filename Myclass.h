4efaaea6ce53d1addb03c8b85dd6eadd7ac1af7e
alirezarahatloo <rahatlooalireza@gmail.com>
2022-12-21 16:52:04 +0330

    Update Myclass.h

--- Myclass.h
+++ Myclass.h
@@ -6,11 +6,35 @@ class MyClass
     public :
         MyClass(int a,int b);
         void printInfo();
+        void setVar(int s);
+        int  getVar();
     private:
         int regVar;
         const int constVar;
         friend void shift(MyClass &p,int step); //friens function allow to change private variable.
 };
 
+class BaseClass
+{
+    public :
+        int var_base;
+        BaseClass();
+        void printff();
+        virtual void print_alireza() = 0; //The 0 tells the compiler that the function has no body.avoid redefined in a derived classمنظور اینه که کلاسهایی که ارث میبرنن باید خودشون تعریف کنن و تعریفش اجباری
+        //virtual void print_alireza(){}
+    protected:
+};
+
+
+class HerClass:public BaseClass
+{
+    public:
+        HerClass();
+        int var1;
+        void printff();
+        void print_alireza();
+    protected:
+        int var2;
+};
 
 #endif
